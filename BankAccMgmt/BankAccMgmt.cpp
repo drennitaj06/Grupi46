4502644d7ee88a7fd708f6372e626af4f6d631e8
Diart7 <144059620+Diart7@users.noreply.github.com>
2025-02-13 23:57:30 +0100

    Funksioni main

--- BankAccMgmt/BankAccMgmt.cpp
+++ BankAccMgmt/BankAccMgmt.cpp
@@ -190,5 +190,27 @@ void transfero() {
 
 int main()
 {
-    // Pika e hyrjes së programit, për momentin bosh
-}
+    //Funksioni main
+        int zgjedhja;
+
+        do {
+            cout << "\n1. Hap Llogari\n2. Depozito\n3. Terheq\n4. Kontrollo Bilancin\n5. Shfaq Detajet\n6. Transfero Para\n7. Dil\nZgjedhja juaj: ";
+            cin >> zgjedhja;
+
+            switch (zgjedhja) {
+            case 1: krijoLlogari(); break;
+            case 2: depozito(); break;
+            case 3: terheq(); break;
+            case 4: shfaqBilancin(); break;
+            case 5: shfaqDetajet(); break;
+            case 6: transfero(); break;
+            case 7:
+                cout << "Dalje...\n";
+                return 0;
+            default:
+                cout << "Zgjedhje e pavlefshme!\n";
+            }
+        } while (zgjedhja != 8);
+
+        return 0;
+    }
